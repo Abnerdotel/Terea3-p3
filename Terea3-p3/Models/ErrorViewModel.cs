d5534b91e65cff3792aa80a4c4e01e77a91f514e
Abner David Dotel Chalas <dotelchalas@gmail.com>
2024-11-22 22:57:03 -0400

    Agregar archivos de proyecto.

---
+++ Terea3-p3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Terea3_p3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
